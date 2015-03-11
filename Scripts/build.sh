cba158e72473fc40bc37a9dd98414c2503cf6194
Jonathan Porta <jonathan@jonathanPorta.com>
2015-03-10 22:17:12 -0700

    Debugging

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -2,6 +2,6 @@
 
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
-  -projectPath /Users/portaj/devel/ci-build \
-  -buildWindowsPlayer /Users/portaj/devel/ci-build/Build/win64/ci-build.exe \
+  -projectPath $(pwd) \
+  -buildWindowsPlayer $(pwd)/Build/win64/ci-build.exe \
   -quit
