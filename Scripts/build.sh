22d228e1459c5b9ac53d15888d9a36e602caf030
Jonathan Porta <jonathan@jonathanPorta.com>
2015-03-10 22:42:38 -0700

    Debugging

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -3,8 +3,9 @@
 echo 'Attempting to build project.'
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
-  -silent-crashes
-  -logFile $(pwd)/unity.log
+  -nographics \
+  -silent-crashes \
+  -logFile $(pwd)/unity.log \
   -projectPath $(pwd) \
   -buildWindowsPlayer $(pwd)/Build/win64/ci-build.exe \
   -quit
