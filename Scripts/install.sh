995de47f86c42f40be377b25209fa6e98aba1724
Jonathan Porta <jonathan@jonathanPorta.com>
2015-03-10 21:42:14 -0700

    Debugging

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 
+echo 'Downloading from http://netstorage.unity3d.com/unity/5b98b70ebeb9/MacEditorInstaller/Unity.pkg'
 curl -O http://netstorage.unity3d.com/unity/5b98b70ebeb9/MacEditorInstaller/Unity.pkg
 
+echo 'Installing Unity.pkg'
 installer -dumplog -package Unity.pkg -target /
