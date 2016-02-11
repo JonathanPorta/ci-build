d2508a81dce66697d94c1a83c1885a49c891ecfc
Tim Vergenz <tim@udacity.com>
2016-02-10 23:35:51 -0500

    Refactor install script
    It would have gotten a little repetitive. :)

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,7 +1,24 @@
 #! /bin/sh
 
-echo 'Downloading from http://netstorage.unity3d.com/unity/3757309da7e7/MacEditorInstaller/Unity-5.2.2f1.pkg: '
-curl -o Unity.pkg http://netstorage.unity3d.com/unity/3757309da7e7/MacEditorInstaller/Unity-5.2.2f1.pkg
+BASE_URL=http://netstorage.unity3d.com/unity
+HASH=3757309da7e7
+VERSION=5.2.2f1
 
-echo 'Installing Unity.pkg'
-sudo installer -dumplog -package Unity.pkg -target /
+download() {
+  file=$1
+  url="$BASE_URL/$HASH/$package"
+
+  echo "Downloading from $url: "
+  curl -o `basename "$package"` "$url"
+}
+
+install() {
+  package=$1
+  download "$package"
+
+  echo "Installing "`basename "$package"`
+  #sudo installer -dumplog -package `basename "$package"` -target /
+}
+
+install "MacEditorInstaller/Unity-$VERSION.pkg"
+
