1ef87377f84ba85bb00c42543f534c2ae52bd079
Tim Vergenz <tim@udacity.com>
2016-02-11 00:27:01 -0500

    Bump version past 5.3 and add extra components
    This fixes issue #2. The story:
    
    I figured the Unity Download Assistant probably had some index of
    where it was downloading everything from. Sure enough, in `Unity
    Download Assistant.app/Contents/Resources/settings.ini` there was a
    link to a hosted `ini` file which has the actual download information.
    
    Example for the version I bumped to:
    http://netstorage.unity3d.com/unity/e87ab445ead0/unity-5.3.2f1-osx.ini
    
    Once I got there I saw it's just the same standard naming format for
    the other micropackages, and I set it up from there.
    
    Works like a dream. :)

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
 BASE_URL=http://netstorage.unity3d.com/unity
-HASH=3757309da7e7
-VERSION=5.2.2f1
+HASH=e87ab445ead0
+VERSION=5.3.2f1
 
 download() {
   file=$1
@@ -17,8 +17,14 @@ install() {
   download "$package"
 
   echo "Installing "`basename "$package"`
-  #sudo installer -dumplog -package `basename "$package"` -target /
+  sudo installer -dumplog -package `basename "$package"` -target /
 }
 
+# See $BASE_URL/$HASH/unity-$VERSION-$PLATFORM.ini for complete list
+# of available packages, where PLATFORM is `osx` or `win`
+
 install "MacEditorInstaller/Unity-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Mac-Support-for-Editor-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Linux-Support-for-Editor-$VERSION.pkg"
 
