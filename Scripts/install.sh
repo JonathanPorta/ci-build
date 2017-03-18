751eb7f6ba98c7adc3d5612d732fb601d01588a2
Ben Bodenmiller <bbodenmiller@hotmail.com>
2017-03-18 09:17:39 -0700

    install npm

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -49,8 +49,8 @@ elif [[ $TRAVIS_OS_NAME == "linux" ]]; then
   #echo 'upgrade all'
   #sudo apt-get upgrade
 
-  echo 'remove node'
-  sudo apt-get --purge autoremove nodejs
+  echo 'install npm'
+  sudo apt-get install npm
 
   echo 'Install Mono'
   sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
