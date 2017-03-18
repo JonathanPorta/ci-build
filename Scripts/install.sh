a9841c9a023f5158f92c783c90c6f2a228919e2e
Ben Bodenmiller <bbodenmiller@hotmail.com>
2017-03-18 07:39:29 -0700

    mono

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -48,6 +48,11 @@ elif [[ $TRAVIS_OS_NAME == "linux" ]]; then
   # Linux install is a bit different
   # latest Linux Unity details can be found at https://forum.unity3d.com/threads/unity-on-linux-release-notes-and-known-issues.350256/
   echo 'Installing Unity on Linux'
+  echo 'Install Mono'
+  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+  echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
+  sudo apt-get update
+
   curl -o unity.deb http://beta.unity3d.com/download/b9488c3b1f9f/unity-editor_amd64-5.6.0xb10Linux.deb
   # from http://askubuntu.com/a/841240/310789
   echo 'try first install'
