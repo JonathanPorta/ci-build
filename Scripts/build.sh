e185c06c908940dd85304576d727948f474f6ad9
Ben Bodenmiller <bbodenmiller@hotmail.com>
2017-03-18 06:47:09 -0700

    simplify build.sh

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -11,15 +11,7 @@ if [ $PLATFORM == "WINDOWS" ]; then
     -projectPath $(pwd) \
     -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
     -quit
-
-  exitcode="$?"
-  if [ $exitcode != 0 ]; then
-    echo "Build failed!"
-    exit "$exitcode"
-  fi
-fi
-
-if [ $PLATFORM == "MACOS" ]; then
+elif [ $PLATFORM == "MACOS" ]; then
   echo "Attempting to build $project for macOS"
   ./Scripts/unity_stdout.sh \
     -batchmode \
@@ -28,15 +20,7 @@ if [ $PLATFORM == "MACOS" ]; then
     -projectPath $(pwd) \
     -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
     -quit
-
-  exitcode="$?"
-  if [ $exitcode != 0 ]; then
-    echo "Build failed!"
-    exit "$exitcode"
-  fi
-fi
-
-if [ $PLATFORM == "LINUX" ]; then
+elif [ $PLATFORM == "LINUX" ]; then
   echo "Attempting to build $project for Linux"
   ./Scripts/unity_stdout.sh \
     -batchmode \
@@ -45,10 +29,6 @@ if [ $PLATFORM == "LINUX" ]; then
     -projectPath $(pwd) \
     -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project" \
     -quit
-
-  exitcode="$?"
-  if [ $exitcode != 0 ]; then
-    echo "Build failed!"
-    exit "$exitcode"
-  fi
 fi
+
+exit "$exitcode"
