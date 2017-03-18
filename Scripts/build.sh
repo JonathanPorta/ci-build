3a627ff8c07fd2b394040a31d4227941cc606d3c
Ben Bodenmiller <bbodenmiller@hotmail.com>
2017-03-18 04:01:48 -0700

    try matrix

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,48 +1,54 @@
-#! /bin/bash
+#!/bin/bash
 
 project="ci-build"
 
-echo "Attempting to build $project for Windows"
-./Scripts/unity_stdout.sh \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -projectPath $(pwd) \
-  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
-  -quit
-  
-exitcode="$?"
-if [ $exitcode != 0 ]; then
-  echo "Build failed!"
-  exit "$exitcode"
+if [ $PLATFORM == "WINDOWS" ]]; then
+  echo "Attempting to build $project for Windows"
+  ./Scripts/unity_stdout.sh \
+    -batchmode \
+    -nographics \
+    -silent-crashes \
+    -projectPath $(pwd) \
+    -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
+    -quit
+
+  exitcode="$?"
+  if [ $exitcode != 0 ]; then
+    echo "Build failed!"
+    exit "$exitcode"
+  fi
 fi
 
-echo "Attempting to build $project for OS X"
-./Scripts/unity_stdout.sh \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -projectPath $(pwd) \
-  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
-  -quit
+if [ $PLATFORM == "MACOS" ]]; then
+  echo "Attempting to build $project for macOS"
+  ./Scripts/unity_stdout.sh \
+    -batchmode \
+    -nographics \
+    -silent-crashes \
+    -projectPath $(pwd) \
+    -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
+    -quit
 
-exitcode="$?"
-if [ $exitcode != 0 ]; then
-  echo "Build failed!"
-  exit "$exitcode"
+  exitcode="$?"
+  if [ $exitcode != 0 ]; then
+    echo "Build failed!"
+    exit "$exitcode"
+  fi
 fi
 
-echo "Attempting to build $project for Linux"
-./Scripts/unity_stdout.sh \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -projectPath $(pwd) \
-  -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project" \
-  -quit
-  
-exitcode="$?"
-if [ $exitcode != 0 ]; then
-  echo "Build failed!"
-  exit "$exitcode"
+if [ $PLATFORM == "LINUX" ]]; then
+  echo "Attempting to build $project for Linux"
+  ./Scripts/unity_stdout.sh \
+    -batchmode \
+    -nographics \
+    -silent-crashes \
+    -projectPath $(pwd) \
+    -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project" \
+    -quit
+
+  exitcode="$?"
+  if [ $exitcode != 0 ]; then
+    echo "Build failed!"
+    exit "$exitcode"
+  fi
 fi
