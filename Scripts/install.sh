ebd4afc96c42d806f559aa42e9e5420773157b78
Ben Bodenmiller <bbodenmiller@hotmail.com>
2017-03-19 02:46:57 -0700

    try 5.3

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -47,24 +47,16 @@ elif [[ $TRAVIS_OS_NAME == "linux" ]]; then
   # latest Linux Unity details can be found at https://forum.unity3d.com/threads/unity-on-linux-release-notes-and-known-issues.350256/
   echo 'Installing Unity on Linux'
 
-  # echo "travis_fold:start:install_nodejs"
-  # echo 'Installing nodejs'
-  # # Unity requires it and isn't installing it properly
-  # curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-  # sudo apt-get install -y nodejs
-  # #sudo apt-get install -y build-essential
-  # echo "travis_fold:end:install_nodejs"
-
-  #echo 'Installing Mono'
-  #sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-  #echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
-  #sudo apt-get update
-  #sudo apt-get install mono-complete gnome-sharp2
-  #sudo apt-get install lib32stdc++6 libpq5
+  echo "travis_fold:start:install_nodejs"
+  echo 'Installing nodejs'
+  # Unity requires it and isn't installing it properly
+  curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+  sudo apt-get install -y nodejs
+  echo "travis_fold:end:install_nodejs"
 
   echo "travis_fold:start:install_unity"
   echo 'Installing Unity'
-  curl -o unity.deb http://beta.unity3d.com/download/b9488c3b1f9f/unity-editor_amd64-5.6.0xb10Linux.deb
+  curl -o unity.deb http://download.unity3d.com/download_unity/linux/unity-editor-5.3.6f1+20160720_amd64.deb
   # from http://askubuntu.com/a/841240/310789
   sudo dpkg -i unity.deb
   echo "travis_fold:end:install_unity"
@@ -72,10 +64,6 @@ elif [[ $TRAVIS_OS_NAME == "linux" ]]; then
   echo "travis_fold:start:install_missing_dependencies"
   echo 'Installing missing dependencies'
   sudo apt-get install -f
-  #echo 'try second install'
-  #sudo dpkg -i unity.deb
-  #echo 'install dep 2'
-  #sudo apt-get install -f
   echo "travis_fold:end:install_missing_dependencies"
 else
   echo 'Unsupported OS'
