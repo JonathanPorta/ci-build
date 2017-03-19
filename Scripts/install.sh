1d9cfdf318a29827f1aaa560faad480270e9550d
Ben Bodenmiller <bbodenmiller@hotmail.com>
2017-03-18 23:33:06 -0700

    node + mono + unity + dep + unity + dep

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -51,11 +51,11 @@ elif [[ $TRAVIS_OS_NAME == "linux" ]]; then
   # Unity requires it and isn't installing it properly
   curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
   sudo apt-get install -y nodejs
-  sudo apt-get install -y build-essential
+  #sudo apt-get install -y build-essential
 
-  #echo 'Installing Mono'
-  #sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-  #echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
+  echo 'Installing Mono'
+  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+  echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
   #sudo apt-get update
   #sudo apt-get install mono-complete gnome-sharp2
   #sudo apt-get install lib32stdc++6 libpq5
